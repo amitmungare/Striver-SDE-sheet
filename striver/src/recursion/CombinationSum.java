72f9d5f5e20734bdccd7909438b748d510895ad6
Amit Mungare <amungare27@gmail.com>
2021-07-26 23:39:39 +0530

    recursion

--- striver/src/recursion/CombinationSum.java
+++ striver/src/recursion/CombinationSum.java
@@ -1,6 +1,28 @@
 package recursion;
-
+import java.util.*;
 public class CombinationSum {
+	
+	 public void comb(int i, int[] arr,int t, List<List<Integer>> ans, List<Integer>ds){
+	        if(i==arr.length){
+	            if(t==0){
+	                ans.add(new ArrayList<>(ds));
+	            }
+	            return;
+	        }
+	        
+	        if(arr[i] <= t){
+	            ds.add(arr[i]);
+	            comb(i, arr, t-arr[i], ans, ds);
+	            ds.remove(ds.size()-1);
+	        }
+	        comb(i+1, arr, t, ans, ds);
+	    }
+	    
+	    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+	        List<List<Integer>> ans = new ArrayList<>();
+	        comb(0, candidates, target, ans, new ArrayList<>());
+	        return ans;
+	    }
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
